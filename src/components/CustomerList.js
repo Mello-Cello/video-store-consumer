7e5112e43c7d7736fd7312479f046c41ba639cd4
Mello-Cello <celloelle@gmail.com>
2019-06-26 10:33:10 -0700

    Wrote CustomerList component to show each customer's name and a select button on the Customer List page.

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import { createDecipher } from 'crypto';
+import { createDecipher } from 'crypto'; // do we need this?
 
 class CustomerList extends Component {
     //pass in props?
@@ -21,19 +21,19 @@ class CustomerList extends Component {
         axios.get(customersURL)
             .then((response) => {
                 console.log(response.data);
-                const customers = response.data.map((CustInfo) => {
+                const customers = response.data.map((custInfo) => {
+                  // const { name, registered_at, address, city, state, postal_code, phone, account_credit } = custInfo;
                     return {
-                      "name": "Shelley Rocha",
-                      "registered_at": "Wed, 29 Apr 2015 07:54:14 -0700",
-                      "address": "Ap #292-5216 Ipsum Rd.",
-                      "city": "Hillsboro",
-                      "state": "OR",
-                      "postal_code": "24309",
-                      "phone": "(322) 510-8695",
-                      "account_credit": 13.15
-                      extrenal_id: movieInfo.extrenal_id,
+                      name: custInfo.name,// string
+                      registered_at: custInfo.registered_at, // Date
+                      address: custInfo.address, // string
+                      city: custInfo.city, //string
+                      state: custInfo.state, // string
+                      postal_code: custInfo.postal_code, // string
+                      phone: custInfo.phone, // string
+                      account_credit: custInfo.account_credit, // number
+                      // external_id: custInfo.external_id, // customers don't have an external id as a field;
                     }
-
                 })
                 this.setState({
                     customers: customers, // only need "customers," since they match?
@@ -42,23 +42,23 @@ class CustomerList extends Component {
             .catch((error) => {
                console.log(error.message)
             });
-    };
+    }
 
     render() {
-       // const eachMovie = this.state.movies.map((movie, i) => {
-       //     return (
-       //     <div>
-       //      <p>{movie.title} <button>Select: {movie.title}</button> </p>
-       //     </div>
-       //      )
-       // })
-       //  const errors = this.state.error;
-       //
-       //  return (
-       //      <section>
-       //          {eachMovie}
-       //      </section>
-       //  )
+       const renderedCustomers = this.state.customers.map((customer, i) => {
+           return (
+           <div key={i}>
+            <p>{customer.name} <button>Select</button> </p>
+           </div>
+            )
+       })
+        // const errors = this.state.error;
+
+        return (
+            <section>
+                {renderedCustomers}
+            </section>
+        )
     }
 
 
